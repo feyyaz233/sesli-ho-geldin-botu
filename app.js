bd10b42183aac929fa823f7f4f0c41d70c47ccd1
Glitch (nova-sage-asparagus) <none>
2021-03-16 21:59:47 +0000

    🍰🐘 Checkpoint ./lozayar.json:24819666/193 ./app.js:24819666/117

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ Voice.on("voiceStateUpdate", async(oldState, newState) => {
     }
 });
 
-Voice.login("ODIxNDkzNDYwMTI5Mjg0MTI2.YFEhZw.-rp2EDm2fWYhs4C1SJn4Mt7fgPQ").catch(err => {//token
+Voice.login("OODIxNDkzNDYwMTI5Mjg0MTI2.YFEhZw.MaJL297ZifuEzObTz7BKxxqckTg").catch(err => {//token
     Voice.error("Ses istemcisine bağlanırken bir hata oluştu: " + err.message);
     return Voice.destroy();
 });
